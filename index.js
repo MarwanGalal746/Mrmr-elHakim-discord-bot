d0e03f20984a71cc91f3bb867bd8655cdb2b2572
Marwan Galal <marawangalal84@gmail.com>
2021-04-30 02:18:45 +0000

    editing index.js and create server.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const fetch = require("node-fetch")
 const {Client} = require('discord.js')
 const client  = new Client();
+const server = require('./server.js')
 
 function getQuote() {
   return fetch("https://zenquotes.io/api/random")
@@ -17,9 +18,12 @@ function getQuote() {
 client.on('ready' , () => {})
 client.on('message', (message) => {
     if(message.author.bot) return ;
-    if(message.content === "$inspire") {
+    if(message.content === "$quote") {
       getQuote().then(quote => message.channel.send(quote))
+    } else if(message.content === "$help") {
+      message.channel.send("Just type $quote to get a random quote")
     }
 })
+server()
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
